c7bb5533bb44bfefe9b4aba07a726d5d6a205215
Sjoerd van de Goor <sjoerdvdgoor@hotmail.com>
2022-06-27 17:13:50 +0200

    Eula flag

--- run.py
+++ run.py
@@ -348,14 +348,11 @@ def install_file(filename: str, directory: str, content: str):
 # Install eula.txt, run.bat and update.bat
 def install_files():
 
-    # Check for eula.txt with the content "eula=true"
-    install_file("eula.txt", "", "eula=true")
-
     # Check for run.bat with the content "python " + this_file + " -Xmx8G -Xms8G\nPAUSE"
-    install_file("run.bat", "", "python " + os.path.basename(__file__) + " -Xmx8G -Xms8G\nPAUSE")
+    install_file("run.bat", "", "python " + os.path.basename(__file__) + " -Xmx8G -Xms8G -Dcom.mojang.eula.agree=true\nPAUSE")
 
     # Check for update.bat with the content "python " + this_file + " -u -Xmx8G -Xms8G\nPAUSE"
-    install_file("update.bat", "", "python " + os.path.basename(__file__) + " -u -Xmx8G -Xms8G\nPAUSE")
+    install_file("update.bat", "", "python " + os.path.basename(__file__) + " -u -Xmx8G -Xms8G -Dcom.mojang.eula.agree=true\nPAUSE")
 
     # Check for clean.bat with the content "python " + this_file + " -c\nPAUSE"
     install_file("clean.bat", "", "python " + os.path.basename(__file__) + " -c")
