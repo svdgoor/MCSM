bfeec8d8b5d969080e264fba48dbda57f3366c5a
Sjoerd van de Goor <sjoerdvdgoor@hotmail.com>
2023-01-26 02:36:38 +0100

    Patch scripts 2

--- clean.bat
+++ clean.bat
@@ -1,4 +1,4 @@
 echo Cleaning up the build directory...
 echo If there is no executable at the location specified, run setup.bat first.
-cd target/build/release
+cd target/release
 mcsm.exe -e
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 echo Updating the server...
 echo If there is no executable at the location specified, run setup.bat first.
-cd target/build/release
+cd target/release
 mcsm.exe -u
